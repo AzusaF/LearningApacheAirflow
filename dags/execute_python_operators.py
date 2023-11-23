2cad2636476de7ee9c4b0eb08a0f859f90ac133d
Azusa Fukuda <afukuda@mySeneca.ca>
2023-11-22 21:35:32 -0500

    Passing parameters to Python callables

--- dags/execute_python_operators.py
+++ dags/execute_python_operators.py
@@ -11,46 +11,32 @@ default_args = {
     'owner' : 'loonycorn'
 }
 
-def task_a():
-    print("TASK A executed!")
+def greet_hello(name):
+    print("Hello, {name}!".format(name=name))
 
-def task_b():
-    time.sleep(5)
-    print("TASK B executed!")
+def greet_hello_with_city(name, city):
+    print("Hello, {name} from {city}".format(name=name, city=city))
 
-def task_c():
-    print("TASK C executed!")
-
-def task_d():
-    print("TASK D executed!")            
 
 with DAG(
     dag_id = 'execute_python_operators',
-    description = 'Python operators in DAGs',
+    description = 'Python operators in DAGs with parameters',
     default_args = default_args,
     start_date = days_ago(1),
     schedule_interval = '@daily',
-    tags = ['dependencies', 'python']
+    tags = ['parameters', 'python']
 ) as dag:
     taskA = PythonOperator(
-        task_id = 'taskA',
-        python_callable = task_a
+        task_id = 'greet_hello',
+        python_callable = greet_hello,
+        op_kwargs={'name': 'Desmond'}
     )
 
     taskB = PythonOperator(
-        task_id = 'taskB',
-        python_callable = task_b
+        task_id = 'greet_hello_with_city',
+        python_callable = greet_hello_with_city,
+        op_kwargs={'name': 'Louise', 'city': 'Seattle'}
     )
 
-    taskC = PythonOperator(
-        task_id = 'taskC',
-        python_callable = task_c
-    )
 
-    taskD = PythonOperator(
-        task_id = 'taskD',
-        python_callable = task_d
-    )
-
-taskA >> [taskB, taskC]
-[taskB, taskC] >> taskD
+taskA >> taskB
