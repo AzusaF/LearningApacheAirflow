a49ba00ff06ecc3bca0451a487d1ac5a57fb30ef
Azusa Fukuda <afukuda@mySeneca.ca>
2023-11-22 22:32:37 -0500

    added some output

--- dags/executing_python_pipeline.py
+++ dags/executing_python_pipeline.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
@@ -11,6 +12,7 @@ default_args = {
    'owner': 'loonycorn'
 }
 
+
 def read_csv_file():
     df = pd.read_csv('/Users/az/airflow/datasets/insurance.csv')
 
@@ -19,17 +21,74 @@ def read_csv_file():
     return df.to_json()
 
 
+def remove_null_values(ti):
+    json_data = ti.xcom_pull(task_ids='read_csv_file')
+    
+    df = pd.read_json(json_data)
+    
+    df = df.dropna()
+
+    print(df)
+
+    return df.to_json()
+
+
+def groupby_smoker(ti):
+    json_data = ti.xcom_pull(task_ids='remove_null_values')
+    df = pd.read_json(json_data)
+
+    smoker_df = df.groupby('smoker').agg({
+        'age': 'mean', 
+        'bmi': 'mean',
+        'charges': 'mean'
+    }).reset_index()
+
+    smoker_df.to_csv(
+        '/Users/az/airflow/output/grouped_by_smoker.csv', index=False)
+
+
+def groupby_region(ti):
+    json_data = ti.xcom_pull(task_ids='remove_null_values')
+    df = pd.read_json(json_data)
+
+    region_df = df.groupby('region').agg({
+        'age': 'mean', 
+        'bmi': 'mean', 
+        'charges': 'mean'
+    }).reset_index()
+    
+
+    region_df.to_csv(
+        '/Users/az/airflow/output/grouped_by_region.csv', index=False)
+
+
 with DAG(
-    'python_pipeline',
-    description='Running a Python pipeline',
-    default_args=default_args,
-    start_date=days_ago(1),
-    schedule_interval='@once',
+    dag_id = 'python_pipeline',
+    description = 'Running a Python pipeline',
+    default_args = default_args,
+    start_date = days_ago(1),
+    schedule_interval = '@once',
     tags = ['python', 'transform', 'pipeline']
 ) as dag:
+    
     read_csv_file = PythonOperator(
         task_id='read_csv_file',
         python_callable=read_csv_file
     )
 
-read_csv_file
+    remove_null_values = PythonOperator(
+        task_id='remove_null_values',
+        python_callable=remove_null_values
+    )
+    
+    groupby_smoker = PythonOperator(
+        task_id='groupby_smoker',
+        python_callable=groupby_smoker
+    )
+    
+    groupby_region = PythonOperator(
+        task_id='groupby_region',
+        python_callable=groupby_region
+    )
+
+read_csv_file >> remove_null_values >> [groupby_smoker, groupby_region]
