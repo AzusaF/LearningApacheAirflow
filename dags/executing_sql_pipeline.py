47eb3b0bae671ac5b401121a0915c3126eee246d
Azusa Fukuda <afukuda@mySeneca.ca>
2023-11-24 16:34:15 -0500

    updated sql pipeline

--- dags/executing_sql_pipeline.py
+++ dags/executing_sql_pipeline.py
@@ -32,5 +32,41 @@ with DAG(
         dag = dag,
     )
 
-create_table
 
+    insert_values_1 = SqliteOperator(
+        task_id = 'insert_values_1',
+        sql = r"""
+            INSERT INTO users (name, age, is_active) VALUES 
+                ('Julie', 30, false),
+                ('Peter', 55, true),
+                ('Emily', 37, false),
+                ('Katrina', 54, false),
+                ('Joseph', 27, true);
+        """,
+        sqlite_conn_id = 'my_sqlite_conn',
+        dag = dag,
+    )
+
+    insert_values_2 = SqliteOperator(
+        task_id = 'insert_values_2',
+        sql = r"""
+            INSERT INTO users (name, age) VALUES 
+                ('Harry', 49),
+                ('Nancy', 52),
+                ('Elvis', 26),
+                ('Mia', 20);
+        """,
+        sqlite_conn_id = 'my_sqlite_conn',
+        dag = dag,
+    )
+
+    display_result = SqliteOperator(
+        task_id = 'display_result',
+        sql = r"""SELECT * FROM users""",
+        sqlite_conn_id = 'my_sqlite_conn',
+        dag = dag,
+        do_xcom_push = True
+    )
+
+
+create_table >> [insert_values_1, insert_values_2] >> display_result
